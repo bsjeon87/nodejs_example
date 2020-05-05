e7cc44438c489071cf96934f203b6e484c469272
Jeon <jbsgo87@gmail.com>
2020-05-05 21:28:34 +0900

    added the moduel

--- first-app/logger2.js
+++ first-app/logger2.js
@@ -1,3 +1,8 @@
+// 실제 모듈은 내부적으로 아래와같이 function 함수형태임.
+//(function (exports, require, module, __filename, __dirname) {
+console.log(__filename);
+console.log(__dirname);
+console.log(module);
 var url = "http://mylogger.io/log";
 
 function log(message) {
@@ -6,3 +11,4 @@ function log(message) {
 }
 
 module.exports = log;
+//});
