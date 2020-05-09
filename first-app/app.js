0b36476f738cd7531c1ea14b74e43d17185d845e
Jeon <jbsgo87@gmail.com>
2020-05-09 22:34:48 +0900

    changed the server code to use req/res event

--- first-app/app.js
+++ first-app/app.js
@@ -29,11 +29,13 @@ logger.log("message");
 */
 
 const http = require("http");
-const server = http.createServer();
-
-server.on("connection", (socket) => {
-  console.log("New connection");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("hello world");
+    res.end();
+  }
 });
+
 server.listen(3000);
 
 console.log("Listening on port 3000...");
