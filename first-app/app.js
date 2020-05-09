952df825c364310133a15a4bd96630bb98bd569f
Jeon <jbsgo87@gmail.com>
2020-05-09 22:32:35 +0900

    added the web server listener event example

--- first-app/app.js
+++ first-app/app.js
@@ -15,7 +15,7 @@ fs.readdir("./", function (err, files) {
   if (err) console.log("Error", err);
   else console.log("Result", files);
 }); */
-
+/*
 const Logger = require("./logger");
 const logger = new Logger();
 logger.on("meesageLogged", (arg) => {
@@ -26,3 +26,14 @@ logger.on("meesageLogged", (arg) => {
 });
 
 logger.log("message");
+*/
+
+const http = require("http");
+const server = http.createServer();
+
+server.on("connection", (socket) => {
+  console.log("New connection");
+});
+server.listen(3000);
+
+console.log("Listening on port 3000...");
