754d4e9bbfb641f70bfd7925074e8e7a3eb623aa
Jeon <jbsgo87@gmail.com>
2020-05-09 22:36:42 +0900

    added the api example

--- first-app/app.js
+++ first-app/app.js
@@ -34,6 +34,10 @@ const server = http.createServer((req, res) => {
     res.write("hello world");
     res.end();
   }
+  if (req.url == "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
 server.listen(3000);
