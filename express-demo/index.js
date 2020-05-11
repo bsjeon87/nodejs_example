373d3bcb2b84d5de4b385a43efae1b85d9083ac1
Jeon <jbsgo87@gmail.com>
2020-05-12 06:26:57 +0900

    about route

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,5 +9,15 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  //localhost:3000/api/posts/2018/1?sortBy=name
+  //  res.send(req.params); // 2018,1
+  res.send(req.query); // sortBy=name
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
